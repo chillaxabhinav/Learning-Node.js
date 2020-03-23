646ab70eb4984831de192d5b84277bf4d4893b31
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-24 02:35:53 +0530

    separated code in node modules and exported it

--- Section-third/server.js
+++ Section-third/server.js
@@ -1,43 +1,7 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-const server = http.createServer((req,res) =>{
-    const url = req.url;
-    const method = req.method;
-    console.log(url,method);
-    if(url === '/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    if(url ==='/message' && method === 'POST'){
-        const body = [];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end',()=>{
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-        // console.log('I am here');
-        
-        res.statusCode=302;
-        res.setHeader('Location','/');
-        return res.end();
-    }
-    //process.exit();
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>My first page</title></head>');
-    res.write('<body><h1>My first Node js Server</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
