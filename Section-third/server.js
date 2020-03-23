07b5a80bca9d29f3df68e9706dce4c9640e24fa1
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-23 23:14:58 +0530

    read incoming request data stream into buffer and saved it in a txt file

--- Section-third/server.js
+++ Section-third/server.js
@@ -1,7 +1,36 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res) =>{
-    console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    const method = req.method;
+    console.log(url,method);
+    if(url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url ==='/message' && method === 'POST'){
+        const body = [];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end',()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+        // console.log('I am here');
+        
+        res.statusCode=302;
+        res.setHeader('Location','/');
+        return res.end();
+    }
     //process.exit();
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
