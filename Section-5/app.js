5a75384eed86881f2c267d1abc519849c1324a58
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-25 22:05:08 +0530

    every use() method is treated as separate middlewares

--- Section-5/app.js
+++ Section-5/app.js
@@ -11,7 +11,16 @@ app.use((req,res,next) => {
 
 app.use((req,res,next) => {
     console.log('THIS IS MY SECOND MIDDLEWARE');
+    next();
 })
+
+
+// Below my() function is also my middleware function called in line 23
+function my(){
+    console.log('will this work ?');
+}
+
+app.use(my);
 
 
 const server = http.createServer(app);
