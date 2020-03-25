ae6d20fc1bac2164abeb52550b89041dc2f6b2c6
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-26 01:02:36 +0530

    Handling different routes

--- Section-5/app.js
+++ Section-5/app.js
@@ -4,26 +4,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log('THIS IS MY FIRST MIDDLEWARE');
-    next(); // Allow us to continue/jump to next middleware present
+app.use('/',(req,res,next) => {
+    console.log('This always runs');
+    next();
 });
 
-app.use((req,res,next) => {
-    console.log('THIS IS MY SECOND MIDDLEWARE');
-    res.send('<h1>Send from express js</h1>');
+app.use('/yupp' , (req,res,next) =>{ 
+    console.log('This should not run ideally');
+    next();
 })
 
+app.use('/add-product',(req,res,next)=>{
+    console.log('This should run');
+    res.send('<h1>This is add product url</h1>');
+});
 
-// Below my() function is also my middleware function
-// so every use() method is treated as middleware and next() method get us to the next use() method defined
-// function my(req,res,next){
-//     console.log('will this work ?');
-//     res.send('<h1>Send from express js</h1>');
-// }
-// app.use(my);
-
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.use('/',(req,res,next) => {
+    console.log('This is my middleware');
+    res.send('<h1>Send from express js</h1>');
+})
+app.listen(3000);
