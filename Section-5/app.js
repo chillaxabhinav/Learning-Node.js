58e4bd6b6035dbc2cb5ca30d906d4242bb95cde1
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-03-25 21:49:01 +0530

    use() method, middlewares and next() to jump to next middleware

--- Section-5/app.js
+++ Section-5/app.js
@@ -2,7 +2,18 @@ const http = require('http');
 
 const express = require('express');
 
+const app = express();
 
-const server = http.createServer();
+app.use((req,res,next) => {
+    console.log('THIS IS MY FIRST MIDDLEWARE');
+    next();
+});
+
+app.use((req,res,next) => {
+    console.log('THIS IS MY SECOND MIDDLEWARE');
+})
+
+
+const server = http.createServer(app);
 
 server.listen(3000);
