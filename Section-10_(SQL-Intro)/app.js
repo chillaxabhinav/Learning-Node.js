d51c1c38f4645e9173894fd9df34aaf66aaf071c
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-12 12:36:03 +0530

    MySql connection pool made

--- Section-10_(SQL-Intro)/app.js
+++ Section-10_(SQL-Intro)/app.js
@@ -7,11 +7,20 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
 app.set('view engine','ejs');
 app.set('views','views');
+
+db.execute('SELECT * FROM products')
+    .then((result) => {
+        console.log('result[0]',result[0]);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
 
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(express.static(path.join(__dirname,'public')));
