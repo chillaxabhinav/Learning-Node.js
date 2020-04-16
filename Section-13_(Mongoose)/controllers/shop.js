f607249c6d4077792d655382de4f5ea6b8a29520
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-16 20:04:11 +0530

    Add items to cart and remove items to cart

--- Section-13_(Mongoose)/controllers/shop.js
+++ Section-13_(Mongoose)/controllers/shop.js
@@ -40,7 +40,11 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => { 
-    req.user.getCart().then(products => {
+    req.user
+        .populate('cart.items.productId')
+        .execPopulate()
+        .then(user => {
+            products = user.cart.items;
             res.render('shop/cart.ejs', {
                 pageTitle: 'Your Cart',
                 path: '/cart',
@@ -68,7 +72,7 @@ exports.postCart = (req,res,next) => {
 
 exports.postCartDeleteProduct = (req,res,next) =>{
     const prodId = req.body.productId;
-    req.user.deleteItemFromCart(prodId)
+    req.user.removeFromCart(prodId)
         .then(result => {
             res.redirect('/cart');
         })
--- Section-13_(Mongoose)/models/product.js
+++ Section-13_(Mongoose)/models/product.js
@@ -26,6 +26,7 @@ const productSchema = new Schema({
 	}
 });
 
+
 module.exports = mongoose.model('Product', productSchema);
 
 
