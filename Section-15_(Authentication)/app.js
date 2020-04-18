194bc66fa9177b5acab8acb3725a9a6b87f187f7
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-18 15:47:32 +0530

    Section-15(Authentication) started

---
+++ Section-15_(Authentication)/app.js
@@ -0,0 +1,74 @@
+const path = require('path');
+
+const express = require('./node_modules/express');
+const bodyParser = require('./node_modules/body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+const errorController = require('./controllers/error');
+const User = require('./models/user.js');
+
+const MONGODB_URI = 'mongodb://localhost:27017/admin';
+
+const app = express();
+const store = new MongoDBStore({
+    uri : MONGODB_URI,
+    collection : 'sessions'
+});
+
+app.set('view engine','ejs');
+app.set('views','views');
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(session({secret : 'my secret', resave : false, saveUninitialized : false, store : store}));
+
+app.use((req,res,next) => {
+    if(req.session.user){
+        User.findById(req.session.user._id)
+            .then(user => {
+                req.user = user;
+                next();
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+    else{
+        next();
+    }
+});
+
+app.use('/admin',adminRoutes);
+
+app.use(shopRoutes);
+
+app.use(authRoutes);
+
+app.use('/',errorController.get404Page);
+
+mongoose.connect(MONGODB_URI)
+    .then(result => {
+        User.findOne().then(user => {
+            if(!user){
+                const user = new User({
+                    name: 'Abhinav',
+                    email: 'abc@gmail.com',
+                    cart: {
+                        items: []
+                    }
+                });
+                user.save();
+            }
+        })
+
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
---
+++ Section-15_(Authentication)/controllers/auth.js
@@ -0,0 +1,28 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+    res.render('auth/login.ejs',{
+        path : '/login',
+        pageTitle: 'Login',
+        isAuthenticated: false
+    })
+};
+
+exports.postLogin = (req, res, next) => {
+    User.findById('5e985e6a9ebefa371c50e743')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save((err) => {
+                console.log(err);
+                res.redirect('/');
+            });
+        })
+        .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy((err) => {
+        res.redirect('/');
+    });
+}
