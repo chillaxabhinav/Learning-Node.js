b78b6490a586db755503250a42cc13ee97f14dac
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-18 21:38:05 +0530

    Section-15 starter

--- Section-15_(Authentication)/app.js
+++ Section-15_(Authentication)/app.js
@@ -1,74 +1,76 @@
 const path = require('path');
 
-const express = require('./node_modules/express');
-const bodyParser = require('./node_modules/body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
+const MONGODB_URI =
+  'mongodb://localhost:27017/admin';
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
 
-const errorController = require('./controllers/error');
-const User = require('./models/user.js');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
-const MONGODB_URI = 'mongodb://localhost:27017/admin';
-
-const app = express();
-const store = new MongoDBStore({
-    uri : MONGODB_URI,
-    collection : 'sessions'
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
-app.set('view engine','ejs');
-app.set('views','views');
-
-app.use(bodyParser.urlencoded({extended : false}));
-app.use(express.static(path.join(__dirname,'public')));
-app.use(session({secret : 'my secret', resave : false, saveUninitialized : false, store : store}));
-
-app.use((req,res,next) => {
-    if(req.session.user){
-        User.findById(req.session.user._id)
-            .then(user => {
-                req.user = user;
-                next();
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }
-    else{
-        next();
-    }
-});
-
-app.use('/admin',adminRoutes);
-
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-
 app.use(authRoutes);
 
-app.use('/',errorController.get404Page);
+app.use(errorController.get404);
 
-mongoose.connect(MONGODB_URI)
-    .then(result => {
-        User.findOne().then(user => {
-            if(!user){
-                const user = new User({
-                    name: 'Abhinav',
-                    email: 'abc@gmail.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        })
-
-        app.listen(3000);
-    })
-    .catch(err => {
-        console.log(err);
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Max',
+          email: 'max@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
     });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
