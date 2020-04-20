aa8b2a87151fc35a84ced44a8ee234492fc4a574
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-04-20 14:51:09 +0530

    ES version 8

--- Section-15_(Authentication)/app.js
+++ Section-15_(Authentication)/app.js
@@ -54,19 +54,6 @@ app.use('/',errorController.get404Page);
 
 mongoose.connect(MONGODB_URI)
     .then(result => {
-        User.findOne().then(user => {
-            if(!user){
-                const user = new User({
-                    name: 'Abhinav',
-                    email: 'abc@gmail.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        })
-
         app.listen(3000);
     })
     .catch(err => {
--- Section-15_(Authentication)/controllers/auth.js
+++ Section-15_(Authentication)/controllers/auth.js
@@ -5,7 +5,7 @@ exports.getLogin = (req, res, next) => {
         path : '/login',
         pageTitle: 'Login',
         isAuthenticated: false
-    })
+    });
 };
 
 exports.getSignup = (req, res, next) => {
@@ -16,7 +16,29 @@ exports.getSignup = (req, res, next) => {
     });
 };
 
-exports.postSignup = (req, res, next) => { };
+exports.postSignup = (req, res, next) => { 
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    User.findOne({email : email})
+        .then(userDoc => {
+            if(userDoc){
+                return res.redirect('/signup');
+            }
+            const user = new User({
+                email : email,
+                password : password,
+                cart : {items : []}
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.redirect('/login');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
 
 exports.postLogin = (req, res, next) => {
     User.findById('5e985e6a9ebefa371c50e743')
@@ -35,4 +57,4 @@ exports.postLogout = (req, res, next) => {
     req.session.destroy((err) => {
         res.redirect('/');
     });
-}
+};
