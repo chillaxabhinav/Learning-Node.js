56ee8cd82d5280c614c6a8f2c56c21a76f5ceaf1
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-07 17:08:58 +0530

    Section 16 started, nodemailer and nodemailer-sendgrid installed

---
+++ Section-16_(Email)/routes/shop.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const express = require('../node_modules/express');
+
+const router = express.Router();
+
+const shopController = require('../controllers/shop');
+const isAuth = require('../middleware/is-auth');
+
+router.get('/', shopController.getIndex);
+
+router.get('/products',shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', isAuth, shopController.getCart);
+
+router.post('/cart', isAuth, shopController.postCart);
+
+router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
+
+router.get('/orders', isAuth, shopController.getOrders);
+
+router.post('/create-order', isAuth, shopController.postOrder);
+
+
+module.exports = router;
