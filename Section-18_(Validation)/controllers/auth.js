5c16cba8cbe66463d31f2922f4af7e3d220652c5
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-10 12:37:59 +0530

    Keeping old user data

--- Section-18_(Validation)/controllers/auth.js
+++ Section-18_(Validation)/controllers/auth.js
@@ -40,7 +40,12 @@ exports.getSignup = (req, res, next) => {
     res.render('auth/signup', {
         path: '/signup',
         pageTitle: 'Signup',
-        errorMessage: message
+        errorMessage: message,
+        oldInput : {
+            email : '',
+            password : '',
+            confirmPassword : ''
+        }
     });
 };
 
@@ -52,7 +57,12 @@ exports.postSignup = (req, res, next) => {
         return res.status(422).render('auth/signup', {
             path: '/signup',
             pageTitle: 'Signup',
-            errorMessage: errors.array()[0].msg
+            errorMessage: errors.array()[0].msg,
+            oldInput : {
+                email : email,
+                password : password,
+                confirmPassword : req.body.confirmPassword
+            }
         });
     }
     
