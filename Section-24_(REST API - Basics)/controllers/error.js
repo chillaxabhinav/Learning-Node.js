611ec5074afb25dd772541151027e3c2d3197e5a
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-14 20:56:19 +0530

    Initial

--- Section-24_(REST API - Basics)/controllers/error.js
+++
@@ -1,5 +0,0 @@
-exports.get404Page = (req, res, next) => {
-    res.status(404).render('404.ejs', {
-        pageTitle: 'Page Not Found',
-        path: '/404' });
-};
---
+++ Section-24_(REST API - Basics)/controllers/feed.js
@@ -0,0 +1,3 @@
+module.exports.getPost = (req, res, next) => {
+    
+}
