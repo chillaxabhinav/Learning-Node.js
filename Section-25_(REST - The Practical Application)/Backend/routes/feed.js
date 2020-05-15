65a92d0f0c91d05a377891555c92eb961c937384
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-15 18:36:44 +0530

    Backend API Server added

---
+++ Section-25_(REST - The Practical Application)/Backend/routes/feed.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+const feedController = require('../controllers/feed');
+
+// GET /feed/posts
+router.get('/posts', feedController.getPost);
+
+// POST /feed/post
+router.post('/post', feedController.createPost);
+
+
+module.exports = router;
