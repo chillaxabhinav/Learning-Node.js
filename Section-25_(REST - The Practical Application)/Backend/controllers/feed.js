4cb1734781335a4156e4dd80e4adb2533aefc61c
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-18 15:52:30 +0530

    Delete post

--- Section-25_(REST - The Practical Application)/Backend/controllers/feed.js
+++ Section-25_(REST - The Practical Application)/Backend/controllers/feed.js
@@ -134,6 +134,32 @@ module.exports.updatePost = (req, res, next) => {
         })
 };
 
+module.exports.deletePost = (req, res, next) =>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post');
+                error.statusCode = 404;
+                throw error;
+            }
+            // check logged in user
+            
+            clearImage(post.imageUrl);
+            return Post.findByIdAndRemove(postId);
+        })
+        .then(result => {
+            console.log(result);
+            res.status(200).json({message : 'Deleted Post.'});
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+}
+
 const clearImage = filePath =>{
     filePath = path.join(__dirname, '..', filePath);
     fs.unlink(filePath, err => console.log(err));
--- Section-25_(REST - The Practical Application)/Backend/routes/feed.js
+++ Section-25_(REST - The Practical Application)/Backend/routes/feed.js
@@ -26,7 +26,13 @@ router.put(
         body('title').trim().isLength({ min: 5 }),
         body('content').trim().isLength({ min: 5 })
     ],
-    feedController.updatePost)
+    feedController.updatePost);
+
+
+router.delete(
+    '/post/:postId',
+    feedController.deletePost
+);
 
 
 module.exports = router;
