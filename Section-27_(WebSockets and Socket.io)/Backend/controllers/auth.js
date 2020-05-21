7e25b87546060453d974e51cd24738b0af3c779d
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-21 21:18:06 +0530

    WebSockets

---
+++ Section-27_(WebSockets and Socket.io)/Backend/controllers/auth.js
@@ -0,0 +1,77 @@
+const User = require('../models/user');
+const  { validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.signUp = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const error = new Error('Validation failed');
+        error.statusCode = 422;
+        error.data = errors.array();
+        throw error;
+    }
+    const email  = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    bcrypt.hash(password, 12)
+        .then(hashed => {
+            const user = new User({
+                email : email,
+                password : hashed,
+                name : name
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.status(201).json({
+                message : 'User created',
+                userId : result._id
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+};
+
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    User.findOne({email : email})
+        .then(user => {
+            if(!user){
+                const error = new Error('User not found');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+            if(!isEqual){
+                const error = new Error('Wrong password');
+                error.statusCode = 401;
+                throw error;
+            }
+            const token = jwt.sign({
+                email : loadedUser.email,
+                userId : loadedUser._id.toString()
+                },
+                'somesupersecret',
+                {
+                    expiresIn : '1h'
+                }
+            );
+            res.status(200).json({token : token, userId : loadedUser._id.toString()})
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+}
---
+++ Section-27_(WebSockets and Socket.io)/Backend/controllers/feed.js
@@ -0,0 +1,176 @@
+const fs = require('fs');
+const path = require('path');
+
+const  {validationResult} = require('express-validator/check');
+
+const Post = require('../models/post');
+
+module.exports.getPosts = (req, res, next) => {
+    const currentPage = req.query.page;
+    const perPage = 2;
+    let totalItems;
+    Post.find().countDocuments()
+        .then(count => {
+            totalItems = count;
+            return Post.find()
+                        .skip((currentPage - 1) * perPage)
+                        .limit(perPage);
+        })
+        .then(posts => {
+            res.status(200)
+                .json({
+                    message: 'Fetched Posts successfully',
+                    posts: posts,
+                    totalItems : totalItems
+                })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+}
+
+module.exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if(!errors){
+        const error = new Error('Validation failed');
+        error.statusCode = 422;
+        throw error;
+    }
+
+    if(!req.file){
+        const error = new Error('No image provided');
+        error.statusCode = 422;
+        throw error;
+    }
+    const imageUrl = req.file.path.replace("\\","/");
+    const title = req.body.title;
+    const content = req.body.content;
+    
+    // Create Post in DB
+
+    const post = new Post({
+        title : title,
+        content : content,
+        imageUrl : imageUrl,
+        creator : { name : 'Abhinav' }
+    })
+    post.save()
+        .then(result => {
+            // console.log(result);
+            res.status(201).json({
+                message : 'Created Post Successfully',
+                post : result
+            })
+        })
+        .catch(err => {
+            if(!err.statusCode){
+                err.statusCode = 500;
+            }
+            next(err);
+        });
+}
+
+module.exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if(!post){
+                const error = new Error('Could not find post');
+                error.statusCode = 404;
+                throw error;
+            }
+            res.status(200).json({
+                message : 'Post Fetched',
+                post : post
+            });
+        })
+        .catch(err =>{
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+};
+
+
+module.exports.updatePost = (req, res, next) => {
+    const postId = req.params.postId;
+    const errors = validationResult(req);
+    if (!errors) {
+        const error = new Error('Validation failed');
+        error.statusCode = 422;
+        throw error;
+    }
+    const title = req.body.title;
+    const content = req.body.content;
+    let imageUrl = req.body.image;
+    if(req.file){
+        imageUrl = req.file.path;
+    }
+    if(!imageUrl){
+        const error = new Error('No file picked');
+        error.statusCode = 422;
+        throw error;
+    }
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post');
+                error.statusCode = 404;
+                throw error;
+            }
+            if(imageUrl !== post.imageUrl){
+                clearImage(post.imageUrl);
+            }
+            post.title = title;
+            post.content = content;
+            post.imageUrl = imageUrl;
+            return post.save();
+        })
+        .then(result => {
+            res.status(200).json({
+                message : 'Post updated',
+                post : result
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+};
+
+module.exports.deletePost = (req, res, next) =>{
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post');
+                error.statusCode = 404;
+                throw error;
+            }
+            // check logged in user
+            
+            clearImage(post.imageUrl);
+            return Post.findByIdAndRemove(postId);
+        })
+        .then(result => {
+            console.log(result);
+            res.status(200).json({message : 'Deleted Post.'});
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        })
+}
+
+const clearImage = filePath =>{
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
+}
