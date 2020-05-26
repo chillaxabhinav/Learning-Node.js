dbdb80b3860a30dd284e9ab5008f862b25031c05
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-26 15:23:42 +0530

    express graphql added and schemas and resolvers added

--- Section-28_(GraphQL)/Backend/app.js
+++ Section-28_(GraphQL)/Backend/app.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
 // const uuidv4 = require('uuid/v4');
-
+const graphqlHttp = require('express-graphql');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
 const MONGODB_URI = 'mongodb://localhost:27017/messages';
 
 const app = express();
@@ -45,6 +47,11 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
     next();
 })
+
+app.use('/graphql',graphqlHttp({
+    schema : graphqlSchema,
+    rootValue : graphqlResolver
+}));
 
 
 app.use((error , req, res, next) => {
