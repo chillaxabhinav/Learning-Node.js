b6e5bcead7dbe08c9f0d496847735dcd2f4d8191
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-05-27 22:46:41 +0530

    Handlling errors

--- Section-28_(GraphQL)/Backend/app.js
+++ Section-28_(GraphQL)/Backend/app.js
@@ -51,7 +51,20 @@ app.use((req, res, next) => {
 app.use('/graphql',graphqlHttp({
     schema : graphqlSchema,
     rootValue : graphqlResolver,
-    graphiql : true
+    graphiql : true,
+    formatError(err){
+        if(!err.originalError){
+            return err;
+        }
+        const data = err.originalError.data;
+        const message = err.message || 'Error occured';
+        const code = err.originalError.code || 500;
+        return {
+            message : message,
+            status : code,
+            data : data
+        }
+    }
 }));
 
 
--- Section-28_(GraphQL)/Backend/graphql/resolvers.js
+++ Section-28_(GraphQL)/Backend/graphql/resolvers.js
@@ -1,9 +1,23 @@
 const User = require('../models/user');
 const bcrypt = require("bcryptjs");
+const validator = require('validator');
 
 module.exports = {
     createUser : async function( { userInput }, req){
         //const email = userInput.email;
+        const errors = [];
+        if(!validator.isEmail(userInput.email)){
+            errors.push({message : 'Email is invalid'});
+        }
+        if(validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, {min : 5})){
+            errors.push({message : 'Password too short'});
+        }
+        if(errors.length > 0){
+            const error = new Error('Invalid input');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
         const existingUser = await User.findOne({email : userInput.email});
         if(existingUser){
             const error = new Error('User already exist');
